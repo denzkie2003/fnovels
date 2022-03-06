d9e066c2a3179089c588e80b2452be8626a7b6fa
DenZ <83795556+denzkie2003@users.noreply.github.com>
2022-03-06 18:32:29 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  
+  //Check browser if in desktop mode
+  window.deviceCheck = function() {
+    var check = false;
+    if (window.innerWidth > 800) {
+      check = true;
+    }
+    return check;
+  }
+  if(window.deviceCheck()){
+      document.getElementsByTagName("body")[0].style.display="none";
+      window.alert("Sorry but the page is currently not available on desktop mode or higher than 800pixels screen wide.\nIf you want to use this site, you can drog your browsers border to change it size to mobile view.");
+  }
+  
+});
