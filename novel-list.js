4496c0cafcf3b92f8a37a152cdd0ab3120bc351f
DenZ <83795556+denzkie2003@users.noreply.github.com>
2022-03-08 07:04:03 +0800

    Add files via upload

--- novel-list.js
+++ novel-list.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
     const novel = [
+        {title:"The Neighboring Aarya-san who Sometimes Acts Affectionate and Murmuring in Russian",link:"the-neighboring-aarya-san-who-sometimes.html"},
         {
-          title:"Full Clearing Another World under a Goddess with Zero Believers",link:"/full-clearing-another-world-with-a-goddess-with-zero-believer.html"
+          title:"Full Clearing Another World under a Goddess with Zero Believers",link:"full-clearing-another-world-with-a-goddess-with-zero-believer.html"
         },{
-          title:"Dreaming Boy Turned Realist",link:"/Yumemiru-Danshi-wa-Genjitsushugisha-Light-Novel.html"
+          title:"Dreaming Boy Turned Realist",link:"Yumemiru-Danshi-wa-Genjitsushugisha-Light-Novel.html"
         },{
-           title:"For Some Reason, The School Goddess Likes to Hang Out at My House",link:"/For-Some-Reason-The-School-Goddess-Likes to-Hang-Out-at-My-House.html"
+           title:"For Some Reason, The School Goddess Likes to Hang Out at My House",link:"For-Some-Reason-The-School-Goddess-Likes to-Hang-Out-at-My-House.html"
         }
     ]
     
