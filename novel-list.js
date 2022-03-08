45b02e6724cca2cf0f981ae0d15137ab1603c66d
DenZ <83795556+denzkie2003@users.noreply.github.com>
2022-03-09 04:21:04 +0800

    Add files via upload

--- novel-list.js
+++ novel-list.js
@@ -9,7 +9,12 @@ $(document).ready(function(){
            title:"For Some Reason, The School Goddess Likes to Hang Out at My House",link:"For-Some-Reason-The-School-Goddess-Likes to-Hang-Out-at-My-House.html"
         },{
             title:"I Know That After School, The Saint is More Than Just Noble Light Novel Light Novel",link:"I-Know-That-After-School-The-Saint-is-More-Than-Just-Noble.html"
-        },{title:"Villainess: Reloaded! Blowing Away Bad Ends with Modern Weapons",link:"Villainess-Reloaded-Blowing-Away-Bad-Ends-with-Modern-Weapons.html"}
+        },{title:"Villainess: Reloaded! Blowing Away Bad Ends with Modern Weapons",link:"Villainess-Reloaded-Blowing-Away-Bad-Ends-with-Modern-Weapons.html"},
+        {
+            title:"In The Land Of Leadale",link:"in-the-land-of-leadale.html"
+        },{
+            title:"The World Of Otome Games Is Tough For Mobs",link:"in-the-land-of-leadale.html"
+        }
     ]
     
     let ul = document.getElementById("novelList");
