76b6944256f9d7365eaf96938a48a88df4511f39
DenZ <83795556+denzkie2003@users.noreply.github.com>
2022-03-10 08:41:35 +0800

    Add files via upload

--- novel-list.js
+++ novel-list.js
@@ -23,7 +23,7 @@ $(document).ready(function(){
             title:"My and Her Game War",link:"my-and-her-game-war.html"
         },{
             title:"For Some Reason, a Gal in My Class Became Friends With My Step-sister",link:"For-Some-Reason-a-Gal-in-My-Class-Became-Friends-With-My-Step-sister.html"
-        }
+        },{title:"Spy Classroom",link:"spy-classroom-light-novel.html"}
     ]
     
     let ul = document.getElementById("novelList");
